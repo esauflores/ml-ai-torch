400ff0440f44762e81e1d7059baaf8ab97bef68c
Cesar Flores <esauflores2022@gmail.com>
2025-05-19 15:01:18 +0700

    updated set programs .sh

--- set_programs.sh
+++ set_programs.sh
@@ -5,19 +5,8 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 curl -fsSL https://tailscale.com/install.sh | sh
 
 # Install Caddy 
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install caddy
-
-CADDYFILE="/etc/caddy/Caddyfile"
-REVERSE_PROXY_BLOCK=$':2718 {\n    reverse_proxy localhost:2718\n}'
-
-if ! grep -q ":2718" "$CADDYFILE"; then
-    echo -e "\n$REVERSE_PROXY_BLOCK" | sudo tee -a "$CADDYFILE"
-    sudo systemctl reload caddy
-    echo "Reverse proxy for :2718 added and Caddy reloaded."
-else
-    echo "Reverse proxy for :2718 already exists in $CADDYFILE."
-fi
+# sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+# curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+# curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+# sudo apt update
+# sudo apt install caddy
